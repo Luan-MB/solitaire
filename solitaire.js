fd3ff5ea210f734d3002149b3d419d1a6dd55b4a
Luan-MB <62627604+Luan-MB@users.noreply.github.com>
2021-01-13 17:24:37 -0300

    Algumas funções novas

--- solitaire.js
+++ solitaire.js
@@ -40,87 +40,95 @@ function shuffleDeck(deck) {
     let aux = new Card
 
     for (let i=0; i<deck.length; i++) {
-        let random = Math.floor(Math.random() * 52)
+        let random = Math.floor(Math.random() * deck.length)
         aux = deck[i]
         deck[i] = deck[random]
         deck[random] = aux
     }
-    return deck
+    return deck;
 }
 
-function renderTab(pile) {
-
-    
-    var tab = document.querySelector("#tab"+pile.length);
-
-    for (let i=0; i<pile.length; i++) {
-
-        newCard = document.createElement('div');
-        if (pile[i].naipe == "♥" || pile[i].naipe == "♦") {
-            newCard.className = 'card red';
-        } else {
-            newCard.className = 'card';
-        }
-        tab.appendChild(newCard);
-    }
-    lastChild = tab.lastElementChild;
-    lastChild.innerText = pile[pile.length-1].valor + pile[pile.length-1].naipe;
-    lastChild.className += " flipped";
-}
-
-function fillPile(size,deck) {
+function fillPile(pileSize,deck) {
 
     let pile = [];
     
-    for (let i=0; i<size; i++) {
+    for (let i=0; i<pileSize; i++) {
         card = deck.pop();
         pile.push(card)
     }
     return pile;
 }
 
+function renderPile(pile,pileName,flipLast) {
+
+    var tab = document.querySelector("#"+pileName);
+    
+    if (pile.length > 0) {
+
+        for (let i=0; i<pile.length; i++) {
+
+        newCard = document.createElement('div');
+        newCard.classList.add('card');
+            if (isRed(pile[i])) {
+                newCard.classList.add('red');
+            }
+        tab.appendChild(newCard);
+        }
+        
+        if (flipLast == true) {
+            lastChild = tab.lastElementChild;
+            lastChild.innerText = pile[pile.length-1].nome;
+            lastChild.className += " flipped";
+        } 
+    } else {
+        tab.innerHTML = "";
+    }
+}
+
+// Função que inicia as pilhas de cartas de forma padrão
 function startGame(deck) {
 
     tab1 = fillPile(1,deck);
-    renderTab(tab1);
+    renderPile(tab1,"tab1",true);
     tab2 = fillPile(2,deck);
-    renderTab(tab2);
+    renderPile(tab2,"tab2",true);
     tab3 = fillPile(3,deck);
-    renderTab(tab3);
+    renderPile(tab3,"tab3",true);
     tab4 = fillPile(4,deck);
-    renderTab(tab4);
+    renderPile(tab4,"tab4",true);
     tab5 = fillPile(5,deck);
-    renderTab(tab5);
+    renderPile(tab5,"tab5",true);
     tab6 = fillPile(6,deck);
-    renderTab(tab6);
+    renderPile(tab6,"tab6",true);
     tab7 = fillPile(7,deck);
-    renderTab(tab7);
-    stock = fillPile(deck.length,deck)
+    renderPile(tab7,"tab7",true);
+    stock = fillPile(deck.length,deck);
+    renderPile(stock,"stock",false);
+}
+
+function isRed(card) {
+    
+    if (card.naipe == "♥" || card.naipe == "♦") {
+        return true;
+    }
+    return false;
 }
 
 var deck = createDeck();
 deck = shuffleDeck(deck);
 startGame(deck);
-console.log(tab6);
 
 
-$('.card').click(function() {
-    parent = $(this).parent().attr('id');
-    card = $(this).html();
-    console.log(parent);
-    console.log(card);
-})
+$('#stock').click(function() {
+    if (stock.length > 0) {
+        var card = stock.pop();
+        waste.push(card);
+    }
 
-//const cards = document.querySelectorAll(".lower .card.flipped");
-//console.log(cards);
-//cards.forEach(cards => cards.addEventListener('click', clickFunc));
-//
-//function selectCard(e) {
-//    value = e;
-//    value.style.border = 'yellow 2px solid';
-//}
-
-//function clickFunc(e) {
-//   var value = e;
-//    console.log(value);
-//}
+    else {
+        stock = fillPile(waste.length,waste);
+        stock = shuffleDeck(stock);
+    }
+    renderPile(stock,"stock",false);
+    renderPile(waste,"waste",true);
+});
